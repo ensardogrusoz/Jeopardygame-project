7aec8bb118decbac02f92bd44b9e5c7ba637d816
Ensar Dogrusoz <kaand06@hotmail.com>
2019-10-23 01:01:01 -0400

    finished the random page added cards and called api

--- triviasearch/views.py
+++ triviasearch/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+import requests
+import re
 # Create your views here.
+
+RE_TAGS = re.compile(r'<[^>]+>')
+
+def random(request):
+    api_req = 'http://jservice.io/api/random?'
+    r = requests.get(api_req)
+    trivia_set = r.json()
+    content_list = []
+    for trivia in trivia_set:
+        dict = { 'id': trivia['id'], 'question' : trivia['question'], 'answer' : RE_TAGS.sub('', trivia['answer']), 'category' : trivia['category']['title'] }
+        content_list.append(dict)
+    return render(request, 'trivia/random.html', {'trivia' : content_list})
