48a0010b6b45e463ead776c74c646b478b0803d1
Ensar Dogrusoz <kaand06@hotmail.com>
2019-10-24 01:31:20 -0400

    added a base.html

--- triviasearch/urls.py
+++ triviasearch/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.home, name='home'),
+    path('', views.category, name='home'),
     path('random/', views.random, name='random-trivia')
 ]
--- triviasearch/views.py
+++ triviasearch/views.py
@@ -1,14 +1,31 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from triviasearch.forms import CategoryForm
 import requests
 import re
 # Create your views here.
 
 RE_TAGS = re.compile(r'<[^>]+>')
 
+def category(request):
+    content_list = []
+ 
+    for offset in range(0,10000): 
+            
+        api_req = 'http://jservice.io/api/categories?count=100&offset=' + str(offset)
+        
+        r = requests.get(api_req)
+        category_set = r.json()
+        for category in category_set:          
+            dict = { 'title': category['title'], 'id': category['id'] }            
+            content_list.append(dict)
+  
+        return render(request, 'trivia/home.html', {'categories': content_list})
+        
+
+
 def random(request):
-    api_req = 'http://jservice.io/api/random?'
+    api_req = 'http://jservice.io/api/random'
     r = requests.get(api_req)
     trivia_set = r.json()
     content_list = []
