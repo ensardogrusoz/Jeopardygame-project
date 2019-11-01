4f3d1e5791559c114869998461fb3b70bca1e2b3
Ensar Dogrusoz <kaandogrusoz00@gmail.com>
2019-11-01 17:13:47 -0400

    updated settings

--- jeopartygame/settings.py
+++ jeopartygame/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3^ndjrf%0kt9&*@v5w+73f^8)gzj%81&fg554q-8f4ej(t4vu@'
-
+#SECRET_KEY = '3^ndjrf%0kt9&*@v5w+73f^8)gzj%81&fg554q-8f4ej(t4vu@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jtriviasearch.herokuapp.com/']
 
 
 # Application definition
