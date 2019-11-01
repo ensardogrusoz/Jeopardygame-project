eb8e9f8117f126021396f379cc67b71201326272
Ensar Dogrusoz <kaandogrusoz00@gmail.com>
2019-11-01 17:52:10 -0400

    fixed

--- jeopartygame/wsgi.py
+++ jeopartygame/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jeopardygame.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jeopartygame.settings')
 
 application = get_wsgi_application()
